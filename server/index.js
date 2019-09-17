c2ab50bf50966a74bb3b58437aa54c8e587ceebe
Tate Stone <tatejamesstone@gmail.com>
2019-09-17 09:47:46 -0600

    More work

--- server/index.js
+++ server/index.js
@@ -1,7 +1,17 @@
 require("dotenv").config()
 const express = require('express')
 const session = require('express-session')
-const {SERVER_PORT, CONNECTION_STRING} = process.env
+const {SERVER_PORT, SESSION_SECRET} = process.env
 
 const app = express()
 
+app.use(express.json())
+app.use(
+    session({
+        secret: SESSION_SECRET,
+        resave: false,
+        saveUninitialized: true
+    })
+)
+
+app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
